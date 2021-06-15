9362b00c425ce9c59c7b1eeaa7b7d59a9cc6553c
Marcos <marcos.mpmf43@gmail.com>
2021-06-15 13:06:57 -0300

    Fazendo alterações visuais e adicionando formulário em html

---
+++ js/principal.js
@@ -0,0 +1,13 @@
+var adicionaProduto = [
+    document.querySelector('#adicionaProduto')
+]
+
+console.log(adicionaProduto);
+
+document.querySelector('.form').addEventListener('submit', function(event) {
+/*     alert ('olá') */
+    var criaTr = document.createElement('tr')
+
+})
+
+
